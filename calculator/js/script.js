1ea82357d0de63cfc34767800debe2b70433bfee
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2022-01-10 13:11:42 +0100

    Add files via upload

---
+++ calculator/js/script.js
@@ -0,0 +1,11 @@
+function szamol() {
+    let szam1Szoveg = document.getElementById("szam1").value;
+    console.log(`Első szám: ${szam1Szoveg}`);
+
+    let szam2Szoveg = document.getElementById("szam2").value;
+    console.log(`Második szám: ${szam2Szoveg}`);
+
+
+}
+
+document.getElementById("szam1").focus();
