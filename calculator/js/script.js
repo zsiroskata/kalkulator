0cc38e017e770d397fff2140881c0904f8f46370
Zsíros Kata <Ny19ZsrosK@szgya.hu>
2022-01-11 13:55:56 +0100

    szamolo

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,11 +1,27 @@
 function szamol() {
     let szam1Szoveg = document.getElementById("szam1").value;
-    console.log(`Első szám: ${szam1Szoveg}`);
-
     let szam2Szoveg = document.getElementById("szam2").value;
-    console.log(`Második szám: ${szam2Szoveg}`);
+    let muv = document.getElementById("muvelet").value
 
 
+    let x = parseInt(szam1Szoveg)
+    let y = parseInt(szam2Szoveg)
+    let eredmeny = 0
+
+
+    if (muv === "osszead"){
+        eredmeny = x + y;
+    } else if  (muv === "kivon"){
+        eredmeny = x - y;
+    } else if (muv === "szoroz"){
+        eredmeny = x * y;
+    } else if (muv === "oszt") {
+        eredmeny = x / y;
+    }
+
+    console.log(eredmeny);
 }
+
+
 
 document.getElementById("szam1").focus();
