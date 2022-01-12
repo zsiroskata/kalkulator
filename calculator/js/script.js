4e3486ffae34cbb7610111ada942051f78243308
Zsíros Kata <Ny19ZsrosK@szgya.hu>
2022-01-12 11:17:37 +0100

    kész

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -2,26 +2,37 @@ function szamol() {
     let szam1Szoveg = document.getElementById("szam1").value;
     let szam2Szoveg = document.getElementById("szam2").value;
     let muv = document.getElementById("muvelet").value
+    let kiiras = document.getElementById("kiiras")
+
+    let x = parseInt(szam1Szoveg);
+    let y = parseInt(szam2Szoveg);
+    let eredmeny = 0;
+  
 
 
-    let x = parseInt(szam1Szoveg)
-    let y = parseInt(szam2Szoveg)
-    let eredmeny = 0
-
-
-    if (muv === "osszead"){
+    if (muv === "+"){
         eredmeny = x + y;
-    } else if  (muv === "kivon"){
+        
+    } else if  (muv === "-"){
         eredmeny = x - y;
-    } else if (muv === "szoroz"){
+        
+    } else if (muv === "*"){
         eredmeny = x * y;
-    } else if (muv === "oszt") {
+    
+    } else if (muv === "/") {
         eredmeny = x / y;
+        
     }
+    kiiras.innerText = `${x} ${muv} ${y} = ${eredmeny}`
 
-    console.log(eredmeny);
+    document.getElementById("szam1").value= "";
+    document.getElementById("szam2").value= "";
+    document.getElementById("muvelet").value = "+";
+
+    
+    
+    document.getElementById("szam1").focus();
 }
 
 
-
-document.getElementById("szam1").focus();
+    document.getElementById("szam1").focus();
